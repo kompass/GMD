01aa3ed690bc0de59d79b4cd65ff7573f56ecb5b
Nicolas Kirchner <nicolas.kirchner@orange.com>
2019-05-20 19:56:16 +0200

    Add import disease_clinical_sign - 3

--- src/orpha.py
+++ src/orpha.py
@@ -8,6 +8,8 @@ def import_disease_from_source(path):
             name = row['value']['Name']['text']
 
             link = row['value']['ExpertLink']['text']
+
+            orpha = row['value']['OrphaNumber']
             
             synonyms = []
             syn_count = int(row['value']['SynonymList']['count'])
@@ -17,7 +19,26 @@ def import_disease_from_source(path):
                 for synonym in row['value']['SynonymList']['Synonym']:
                     synonyms.append(synonym['text'])
 
-            yield {'name': name, 'link': link, 'synonyms': synonyms}
+            omim = []
+            ulms = []
+            meddra = []
+            ref_count = int(row['value']['ExternalReferenceList']['count'])
+            refs = []
+            if  ref_count == 1:
+                refs.append(row['value']['SynonymList']['Synonym']['text'])
+            elif ref_count != 0:
+                for synonym in row['value']['SynonymList']['Synonym']:
+                    refs.append(synonym['text'])
+
+            for ref in refs:
+                if ref['Source'] == 'OMIM':
+                    omim.append(ref['Reference'])
+                elif ref['Source'] == 'UMLS':
+                    umls.append(ref['Reference'])
+                elif ref['Source'] == 'MEDDRA':
+                    meddra.append(ref['Reference'])
+
+            yield {'name': name, 'link': link, 'synonyms': synonyms, 'omim': omim, 'ulms': ulms, 'meddra': meddra}
 
 
 def import_disease_clinical_sign_from_source(path):
