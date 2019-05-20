db729b0acce371eaffaedd48436eaa321cf11256
Nicolas Kirchner <nicolas.kirchner@orange.com>
2019-05-20 21:53:00 +0200

    Reindex OMIM and Orpha

--- src/main.py
+++ src/main.py
@@ -5,21 +5,43 @@ from elasticsearch.helpers import bulk
 from omim import import_omim_from_source, import_omim_onto_from_source
 from orpha import import_disease_from_source, import_disease_clinical_sign_from_source
 
+def clean_class_id(record):
+    class_id = record['ClassId']
+    class_id = class_id.split('/')[-1]
+    class_id = class_id.split('.')[0]
+
+    if class_id.startswith('MTHU'):
+        class_id = class_id[4:]
+
+    record['ClassId'] = int(class_id)
+
+    return record
+
 es = Elasticsearch()
 
-omim_mdate = int(os.path.getmtime('Data/omim.txt'))
-print('Last modification of OMIM: {}'.format(omim_mdate))
+print('Reindex OMIM.')
+es.indices.delete(index='omim', ignore=[400, 404])
 
-omim_last_update = es.search(index="source_updates", body={"query": {"term": {"source": "OMIM"}}})
-print('Last indexation of OMIM: {}'.format(omim_last_update))
+records = import_omim_from_source('Data/omim.txt', ['TI', 'CS', 'NO'], ['NO'])
+actions = map(lambda record: {'_index': 'omim', '_type': '_doc', **record}, records)
+bulk(es, actions)
 
-if res['hits']['total']['value'] == 0 || res['hits']['hits'][0]['last_update'] < omim_mdate:
-	print('Reindex OMIM.')
-	es.indices.delete(index='omim', ignore=[400, 404])
+records = import_omim_onto_from_source('Data/omim_onto.csv', {'ClassId': 0, 'PreferredLabel': 1, 'Synonyms': 2, 'CUI': 5})
+records = filter(lambda record: record['ClassId'].startswith('http://purl.bioontology.org/ontology/OMIM'), records)
+records = map(clean_class_id, records)
+actions = map(lambda record: {'_index': 'omim_onto', '_type': '_doc', **record}, records)
+bulk(es, actions)
 
-	records = import_omim_from_source('Data/omim.txt', ['TI', 'CS', 'NO'], ['NO'])
-	actions = map(lambda record: {'_index': 'omim', '_type': '_doc', **record}, records)
-    bulk(es, actions)
+print('Done.')
+print('Reindex Orpha.')
+
+records = import_disease_from_source('Data/disease.json')
+actions = map(lambda record: {'_index': 'disease', '_type': '_doc', **record}, records)
+bulk(es, actions)
+
+records = import_disease_clinical_sign_from_source('Data/disease_clinical_sign.json')
+actions = map(lambda record: {'_index': 'disease_clinical_sign', '_type': '_doc', **record}, records)
+bulk(es, actions)
 
 app = Flask(__name__)
 
