e8ae95f1f8890f770c7f1031de47e666d7f3a2f4
Nicolas Kirchner <nicolas.kirchner@orange.com>
2019-05-20 18:47:44 +0200

    orpha - disease to elastic - 2

--- src/orpha.py
+++ src/orpha.py
@@ -20,6 +20,10 @@ def import_disease_from_source(path):
             yield {'name': name, 'link': link, 'synonyms': synonyms}
 
 if __name__ == '__main__':
+    from elasticsearch import Elasticsearch
+    from elasticsearch.helpers import bulk
+    es = Elasticsearch()
+    
     records = import_disease_from_source('Data/disease.json')
     actions = map(lambda record: {'_index': 'disease', '_type': '_doc', **record}, records)
     bulk(es, actions)
